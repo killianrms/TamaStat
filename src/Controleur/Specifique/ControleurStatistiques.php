b5a2efffc53fae67f666242395e675481ff0387a
Aeroz <killian.ramus@etu.umontpellier.fr>
2025-01-27 09:47:11 +0100

    rollback

--- src/Controleur/Specifique/ControleurStatistiques.php
+++ src/Controleur/Specifique/ControleurStatistiques.php
@@ -1,18 +1,8 @@
 <?php
-class ConnexionUtilisateur {
-    private $pdo;
-
-    public function __construct() {
-        $this->pdo = new PDO('mysql:host=localhost;dbname=ton_db', 'root', '');
-    }
-
-    public function verifierUtilisateur($nom_utilisateur) {
-        $stmt = $this->pdo->prepare('SELECT * FROM utilisateurs WHERE nom_utilisateur = :nom_utilisateur');
-        $stmt->execute(['nom_utilisateur' => $nom_utilisateur]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public function importerCsv() {
+class ControleurStatistiques
+{
+    public function importerCsv()
+    {
         if ($_FILES['fichier']['error'] === UPLOAD_ERR_OK) {
             $cheminFichier = $_FILES['fichier']['tmp_name'];
             $handle = fopen($cheminFichier, 'r');
@@ -30,6 +20,4 @@ class ConnexionUtilisateur {
             exit;
         }
     }
-
 }
-
