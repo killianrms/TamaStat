7f0373d7425df186c8f9e1771a7c39cd0620d9f2
Aeroz <killian.ramus@etu.umontpellier.fr>
2025-01-29 10:57:35 +0100

    feat : mention légal et cgu

--- src/Vue/utilisateur/footer.php
+++ src/Vue/utilisateur/footer.php
@@ -1,11 +1,19 @@
 <footer>
-    <div class="footer-info">
-        <p>Contactez-nous :</p>
-        <ul>
-            <li>Email : <a href="mailto:draguignan@mondialbox.com">draguignan@mondialbox.com</a></li>
-            <li>Adresse : 724 Av. Pierre Brossolette, 83300 Draguignan</li>
-            <li>Téléphone : <a href="tel:+33638466882">+33 6 38 46 68 82</a></li>
-        </ul>
-        <p>&copy; <?php echo date("Y"); ?> TamaBox</p>
+    <div class="footer-container">
+        <div class="footer-info">
+            <p>Contactez-nous :</p>
+            <ul>
+                <li>Email : <a href="mailto:draguignan@mondialbox.com">draguignan@mondialbox.com</a></li>
+                <li>Adresse : 724 Av. Pierre Brossolette, 83300 Draguignan</li>
+                <li>Téléphone : <a href="tel:+33638466882">+33 6 38 46 68 82</a></li>
+            </ul>
+        </div>
+        <div class="footer-links">
+            <a href="mentions-legales.php">Mentions Légales</a> |
+            <a href="cgu.php">Conditions Générales d'Utilisation</a>
+        </div>
+        <div class="footer-copy">
+            <p>&copy; <?php echo date("Y"); ?> TamaBox - Tous droits réservés.</p>
+        </div>
     </div>
 </footer>
