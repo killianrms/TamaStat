63e2954885acea9168ecacfde7049f8a48146c09
Aeroz <killian.ramus@etu.umontpellier.fr>
2025-02-03 09:09:47 +0100

    feat : enlève le bouton accueil de tout le monde

--- src/Vue/utilisateur/header.php
+++ src/Vue/utilisateur/header.php
@@ -2,17 +2,17 @@
     <div class="navbar">
         <h1>TamaStats</h1>
 
+        <?php if (isset($_SESSION['user'])): ?>
         <div class="nav-links">
+
             <a href="routeur.php?route=accueil">Accueil</a>
+            <a href="routeur.php?route=stats">Stats</a>
 
-            <?php if (isset($_SESSION['user'])): ?>
-                <a href="routeur.php?route=stats">Stats</a>
+            <?php if ($_SESSION['user']['is_admin'] === 1): ?>
+                <a href="routeur.php?route=gestionUtilisateurs">Gestion Utilisateurs</a>
+            <?php endif; ?>
 
-                <?php if ($_SESSION['user']['is_admin'] === 1): ?>
-                    <a href="routeur.php?route=gestionUtilisateurs">Gestion Utilisateurs</a>
-                <?php endif; ?>
-
-                <a href="routeur.php?route=deconnexion">Déconnexion</a>
+            <a href="routeur.php?route=deconnexion">Déconnexion</a>
             <?php else: ?>
                 <a href="routeur.php?route=connexion">Connexion</a>
             <?php endif; ?>
