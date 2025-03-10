06af676ad4bd99f6126676242632343dfad5506a
Aeroz <killian.ramus@etu.umontpellier.fr>
2025-03-10 11:09:03 +0100

    feat : refonte du pied de page avec réorganisation des sections et amélioration du style

--- src/Vue/utilisateur/footer.php
+++ src/Vue/utilisateur/footer.php
@@ -1,34 +1,36 @@
 <footer>
     <div class="footer-container">
-        <div class="footer-info">
-            <p>Contactez-nous :</p>
-            <ul>
-                <li>Email : <a href="mailto:draguignan@mondialbox.com">draguignan@mondialbox.com</a></li>
-                <li>Adresse : 724 Av. Pierre Brossolette, 83300 Draguignan</li>
-                <li>Téléphone : <a href="tel:+33638466882">+33 6 38 46 68 82</a></li>
-            </ul>
+        <div class="footer-main">
+            <div class="footer-info">
+                <p>Contactez-nous :</p>
+                <ul>
+                    <li>Email : <a href="mailto:draguignan@mondialbox.com">draguignan@mondialbox.com</a></li>
+                    <li>Adresse : 724 Av. Pierre Brossolette, 83300 Draguignan</li>
+                    <li>Téléphone : <a href="tel:+33638466882">+33 6 38 46 68 82</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-socials">
+                <a href="https://www.facebook.com/MDBDraguignan/" target="_blank" class="social-link">
+                    <img src="../../../ressources/images/facebook.svg" alt="Facebook">
+                </a>
+                <a href="https://www.instagram.com/mondialboxdraguignan/" target="_blank" class="social-link">
+                    <img src="../../../ressources/images/instagram.svg" alt="Instagram">
+                </a>
+                <a href="https://draguignan.mondialbox.com/fr" target="_blank" class="social-link">
+                    <img src="../../../ressources/images/web.svg" alt="Site Web">
+                </a>
+            </div>
         </div>
 
-        <div class="footer-links">
-            <a href="routeur.php?route=mentions-legales">Mentions Légales</a> |
-            <a href="routeur.php?route=cgu">Conditions Générales d'Utilisation</a>
-        </div>
-
-        <!-- 📌 Section Réseaux Sociaux -->
-        <div class="footer-socials">
-            <a href="https://www.facebook.com/MDBDraguignan/" target="_blank" class="social-link">
-                <img src="../../../ressources/images/facebook.svg" alt="Facebook MondialBox Draguignan">
-            </a>
-            <a href="https://www.instagram.com/mondialboxdraguignan/" target="_blank" class="social-link">
-                <img src="../../../ressources/images/instagram.svg" alt="Instagram MondialBox Draguignan">
-            </a>
-            <a href="https://draguignan.mondialbox.com/fr" target="_blank" class="social-link">
-                <img src="../../../ressources/images/web.svg" alt="Site MondialBox Draguignan">
-            </a>
-        </div>
-
-        <div class="footer-copy">
-            <p>&copy; <?php echo date("Y"); ?> TamaBox - Tous droits réservés.</p>
+        <div class="footer-bottom">
+            <div class="footer-links">
+                <a href="routeur.php?route=mentions-legales">Mentions Légales</a> |
+                <a href="routeur.php?route=cgu">Conditions Générales d'Utilisation</a>
+            </div>
+            <div class="footer-copy">
+                <p>&copy;<?php echo date("Y"); ?> TamaBox - Tous droits réservés.</p>
+            </div>
         </div>
     </div>
 </footer>
