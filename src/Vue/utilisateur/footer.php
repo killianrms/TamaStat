bce009c9d6390893c95291805ef8b6aea30ce885
Aeroz <killian.ramus@etu.umontpellier.fr>
2025-03-10 10:59:44 +0100

    feat : ajout des icônes de réseaux sociaux dans le pied de page et styles associés

--- src/Vue/utilisateur/footer.php
+++ src/Vue/utilisateur/footer.php
@@ -8,10 +8,25 @@
                 <li>Téléphone : <a href="tel:+33638466882">+33 6 38 46 68 82</a></li>
             </ul>
         </div>
+
         <div class="footer-links">
             <a href="routeur.php?route=mentions-legales">Mentions Légales</a> |
             <a href="routeur.php?route=cgu">Conditions Générales d'Utilisation</a>
         </div>
+
+        <!-- 📌 Section Réseaux Sociaux -->
+        <div class="footer-socials">
+            <a href="https://www.facebook.com/MDBDraguignan/" target="_blank" class="social-link">
+                <img src="../../../ressources/images/facebook.svg" alt="Facebook MondialBox Draguignan">
+            </a>
+            <a href="https://www.instagram.com/mondialboxdraguignan/" target="_blank" class="social-link">
+                <img src="../../../ressources/images/instagram.svg" alt="Instagram MondialBox Draguignan">
+            </a>
+            <a href="https://draguignan.mondialbox.com/fr" target="_blank" class="social-link">
+                <img src="../../../ressources/images/web.svg" alt="Site MondialBox Draguignan">
+            </a>
+        </div>
+
         <div class="footer-copy">
             <p>&copy; <?php echo date("Y"); ?> TamaBox - Tous droits réservés.</p>
         </div>
